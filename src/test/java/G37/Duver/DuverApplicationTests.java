fc78778862d3ecfbbe3539e5b0a0b8d7de7e3925
DuverneyGomezAguirre <duverney.gomez.mt@corre.usa.edu.co>
2021-10-22 18:41:26 -0500

    first commit

---
+++ src/test/java/G37/Duver/DuverApplicationTests.java
@@ -0,0 +1,13 @@
+package G37.Duver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
