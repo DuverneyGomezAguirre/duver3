4b1319830cd9df2a457b46b759e7b5b79c96308f
DuverneyGomezAguirre <duverney.gomez.mt@corre.usa.edu.co>
2021-10-24 18:26:04 -0500

    first commit

---
+++ src/main/java/G37/Duver/DuverApplication.java
@@ -0,0 +1,13 @@
+package G37.Duver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuverApplication.class, args);
+	}
+
+}
